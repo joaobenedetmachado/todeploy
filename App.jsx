8b76b4517fe0784b92892f6a433b28f5933e6a73
PedroLucaskjkj <81587698+PedroLucaskjkj@users.noreply.github.com>
2022-10-07 14:39:10 -0300

    comito 800vezes por minuto

--- App.jsx
+++ App.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { Provider as PaperProvider, Button } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
