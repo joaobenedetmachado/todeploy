7a9b0c48c9a2bebfe395c103cf997ee0ea78ceac
PedroLucaskjkj <81587698+PedroLucaskjkj@users.noreply.github.com>
2022-10-07 14:45:26 -0300

    agora tudo certinho

--- App.jsx
+++ App.jsx
@@ -5,7 +5,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-const Drawer = createDrawerNavigator();
 
 import Detalhes from './screens/Detalhes';
 
