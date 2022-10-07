afef7f4d4296ae65c1a6e7c063b9d728050b4b1a
PedroLucaskjkj <81587698+PedroLucaskjkj@users.noreply.github.com>
2022-10-07 14:14:01 -0300

    comitei

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { Provider as PaperProvider, Button } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Drawer = createDrawerNavigator();
+
+import Detalhes from './screens/Detalhes';
+
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 21, marginBottom: 16 }}>Tela Inicial</Text>
+      <Button mode="contained" onPress={() => navigation.navigate('Detalhes')}>
+        Ir para Dets
+      </Button>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home" screenOptions={{headerTitleAlign: 'center'}}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Detalhes" component={Detalhes} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
+
---
+++ screens/Detalhes.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-paper';
+
+
+export default function Detalhes({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 21, marginBottom: 16 }}>
+        Outra tela, agora detalhes.
+      </Text>
+      <Button mode="contained" onPress={() => navigation.goBack()}>
+        voltar
+      </Button>
+    </View>
+  );
+}
