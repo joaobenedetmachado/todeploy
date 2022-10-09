2c99a98d1210d603d08fdccc4feea457180da778
PedroLucaskjkj <81587698+PedroLucaskjkj@users.noreply.github.com>
2022-10-09 08:39:51 -0300

    joao onde caralhos esta detalhes joao onde

--- App.jsx
+++ App.jsx
@@ -3,8 +3,9 @@ import { Provider as PaperProvider} from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Problema from './screens/Problema';
 import Login from './screens/Login';
+import Problema from './screens/Problema';
+
 
 const Stack = createNativeStackNavigator();
 
