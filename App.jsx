039a79946d80dcace8891a20dc0fbb2ee5bb1aff
PedroLucaskjkj <81587698+PedroLucaskjkj@users.noreply.github.com>
2022-10-08 22:38:59 -0300

    vai toma no  meu cu no meu cu vai*

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+/*import * as React from 'react';
 import { Provider as PaperProvider} from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -26,3 +26,38 @@ export default function App() {
   );
 }
 
+
+*/
+
+
+
+
+
+import * as React from 'react';
+import { Provider as PaperProvider, Button } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Problema from './screens/Problema';
+import Login from './screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="HarmoFit"
+          screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: { backgroundColor: '#000000' },
+            headerTitleStyle: { fontWeight: 'bold', color: '#fff' },
+          }}>
+          <Stack.Screen name="HarmoFit" component={Login} />
+          <Stack.Screen name="Detalhes" component={Detalhes} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
--- screens/Problema.jsx
+++ screens/Problema.jsx
@@ -1,17 +1,67 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Text, View, StyleSheet, SafeAreaView, Image, Switch} from 'react-native';
+import React, { useState } from "react";
+
+import CardS from "./CardS"
 
 
-export default function Problema({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 21, marginBottom: 16 }}>
-            porfavboooo ajuda
-        </Text>
-        <Button mode="contained" onPress={() => navigation.goBack()}>
-            Autismo
-        </Button>
-      </View>
-    );
+export default function Problema() {
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.paragraph}>Selecione o local da sua dor</Text>
+          </View>
+          <View style={styles.imgesqueleto}>
+            <Image source={require('./esqueleto.png')}/>
+          </View>
+            <View style={styles.switchombro}>
+            <CardS/>
+            </View>
+            <View style={styles.switchcostas}>
+            <CardS/>
+            </View>
+            <View style={styles.switchjoelho}>
+            <CardS/>
+            </View>
+        </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#29abac',
+    height: '100%',
+    width: '100%'
+  },
+  paragraph: {
+    color: "#ffffff",
+    textAlign: 'center',
+    fontSize: 20,
+    fontStyle: 'bold',
+  },
+  header : {
+    height : 120,
+    backgroundColor: '#000000',
+    justifyContent: 'center',
+    borderBottomRightRadius: 30,
+  },
+  imgesqueleto : {
+    marginTop: 80,
+    width: '100%',
+    alignItems: 'center',
+    position: 'absolute',
+  },
+  switchombro : {
+    marginTop: 75,
+    marginLeft: 100,
+  },
+  switchcostas : {
+    marginTop: 95,
+    marginLeft: 150,
+  },
+  switchjoelho : {
+    marginTop: 200,
+    marginLeft: 120,
   }
+});
