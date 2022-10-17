90be6b52849e4ea2d475c791d67c02e12a6a8080
PedroLucaskjkj <pedrolucas1101@proton.me>
2022-10-17 06:54:08 -0300

    commitzinho de cria 7 da manha p arrumar bug

--- screens/Problema.jsx
+++ screens/Problema.jsx
@@ -1,47 +1,10 @@
 import { Text, View, StyleSheet, SafeAreaView, Image, Switch} from 'react-native';
 import React, { useState, isValidElement } from "react";
+import { Button } from 'react-native-paper'
 import Constants from 'expo-constants';
 
-export default function App() {
-    const [isEnabledombro, setIsEnabledombro] = useState(false);
-    const [text, setText] = useState([])
+export default function Problema() {
 
-    const [isEnabledcostas, setIsEnabledcostas] = useState(false);
-
-    const [isEnabledjoelho, setIsEnabledjoelho] = useState(false);
-
-    const toggleSwitchombro = () => {
-      if (isEnabledombro) {
-        setText(current => [...current, '']);
-      }
-      else {
-        setText(current => [...current, 'Ombro']);
-      }
-
-      setIsEnabledombro(previousState => !previousState)
-    }
-    const toggleSwitchcostas = () => {
-      if (isEnabledcostas) {
-        setText(current => [...current, '']);
-      }
-      else {
-        setText(current => [...current, 'Costas']);
-      }
-
-      setIsEnabledcostas(previousState => !previousState)
-    }
-
-    const toggleSwitchjoelho = () => {
-      if (isEnabledjoelho) {
-        setText(current => [...current, '']);
-      }
-      else {
-        setText(current => [...current, 'Joelho']);
-      }
-
-      setIsEnabledjoelho(previousState => !previousState)
-    }
-    
   return (
     <SafeAreaView>
       <View style={styles.container}>
@@ -51,47 +14,9 @@ export default function App() {
           <View style={styles.imgesqueleto}>
             <Image style={{}} source={require('./esqueleto.png')}/>
           </View>
-            <View style={styles.switchombro}>
-            <View>
-              <Switch
-              trackColor={{ false: "#767577", true: "#81b0ff" }}
-              thumbColor={isEnabledombro ? "#f5dd4b" : "#f4f3f4"}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleSwitchombro}
-              value={isEnabledombro}
-              />
-            </View>
-            </View>
-            <View style={styles.switchcostas}>
-            <View>
-              <Switch
-              trackColor={{ false: "#767577", true: "#81b0ff" }}
-              thumbColor={isEnabledcostas ? "#f5dd4b" : "#f4f3f4"}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleSwitchcostas}
-              value={isEnabledcostas}
-              />
-            </View>
-            </View>
-            <View style={styles.switchjoelho}>
-            <View>
-              <Switch
-              trackColor={{ false: "#767577", true: "#81b0ff" }}
-              thumbColor={isEnabledjoelho ? "#f5dd4b" : "#f4f3f4"}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleSwitchjoelho}
-              value={isEnabledjoelho}
-              />
-            </View>
-            </View>
-            <View style={{height: 100, width: '100%', justifyContent:'center', alignItems: 'center', marginTop: 30}}>
-              <View style={{height: 40, width: 150, backgroundColor: '#fff', borderRadius: 10, justifyContent:'center', alignItems: 'center',}}>
-              <Text style={{fontSize: 25}}>{text[0]}</Text>
-              </View>
-              <View style={{height: 40, width: 150, backgroundColor: '#fff', marginTop: 8, borderRadius: 10, justifyContent:'center', alignItems: 'center',}}>
-              <Text style={{fontSize: 25}}>{text[1]}</Text>
-              </View>
-            </View>
+            <Button style={styles.switchombro}>ombro</Button>
+            <Button style={styles.switchcostas}>costas</Button>
+            <Button style={styles.switchjoelho}>joelho</Button>
         </View>
     </SafeAreaView>
   );
