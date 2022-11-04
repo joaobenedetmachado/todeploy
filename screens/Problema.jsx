65286e5df73ad27ae9f54f62a9f5271f67ac6585
jow <jvbm2177@gmail.com>
2022-11-03 21:47:53 -0300

    falta fazer a pagina

--- screens/Problema.jsx
+++ screens/Problema.jsx
@@ -1,10 +1,48 @@
 import { Text, View, StyleSheet, SafeAreaView, Image, Switch} from 'react-native';
 import React, { useState, isValidElement } from "react";
-import { Button } from 'react-native-paper'
 import Constants from 'expo-constants';
+import { Button } from 'react-native-paper';
 
-export default function Problema() {
+export default function App() {
+    const [isEnabledombro, setIsEnabledombro] = useState(false);
+    const [text, setText] = useState([])
 
+    const [isEnabledcostas, setIsEnabledcostas] = useState(false);
+
+    const [isEnabledjoelho, setIsEnabledjoelho] = useState(false);
+
+    const toggleSwitchombro = () => {
+      if (isEnabledombro) {
+        setText(current => [...current, '']);
+      }
+      else {
+        setText(current => [...current, 'Ombro']);
+      }
+
+      setIsEnabledombro(previousState => !previousState)
+    }
+    const toggleSwitchcostas = () => {
+      if (isEnabledcostas) {
+        setText(current => [...current, '']);
+      }
+      else {
+        setText(current => [...current, 'Costas']);
+      }
+
+      setIsEnabledcostas(previousState => !previousState)
+    }
+
+    const toggleSwitchjoelho = () => {
+      if (isEnabledjoelho) {
+        setText(current => [...current, '']);
+      }
+      else {
+        setText(current => [...current, 'Joelho']);
+      }
+
+      setIsEnabledjoelho(previousState => !previousState)
+    }
+    
   return (
     <SafeAreaView>
       <View style={styles.container}>
@@ -12,11 +50,51 @@ export default function Problema() {
             <Text style={styles.paragraph}>Selecione o local da sua dor</Text>
           </View>
           <View style={styles.imgesqueleto}>
-            <Image style={{}} source={require('./esqueleto.png')}/>
+            <Image style={{}} source={require('./esqueletoo.png')}/>
           </View>
-            <Button style={styles.switchombro}>ombro</Button>
-            <Button style={styles.switchcostas}>costas</Button>
-            <Button style={styles.switchjoelho}>joelho</Button>
+            <View style={styles.switchombro}>
+            <View>
+              <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={isEnabledombro ? "#f5dd4b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitchombro}
+              value={isEnabledombro}
+              />
+            </View>
+            </View>
+            <View style={styles.switchcostas}>
+            <View>
+              <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={isEnabledcostas ? "#f5dd4b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitchcostas}
+              value={isEnabledcostas}
+              />
+            </View>
+            </View>
+            <View style={styles.switchjoelho}>
+            <View>
+              <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={isEnabledjoelho ? "#f5dd4b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitchjoelho}
+              value={isEnabledjoelho}
+              />
+            </View>
+            </View>
+            <View style={{height: 100, width: '100%', justifyContent:'center', alignItems: 'center', marginTop: 30}}>
+              <View style={{height: 40, width: 150, backgroundColor: '#fff', borderRadius: 10, justifyContent:'center', alignItems: 'center',}}>
+              <Text style={{fontSize: 25}}>{text[0]}</Text>
+              </View>
+              <View style={{justifyContent:'center', alignItems: 'center',}}>
+              <Button color='#000000' mode="contained" style={{ marginTop: 10, width:150, height: 40, borderRadius: 10, alignItems:'center', justifyContent:'center'}}>
+    Continuar
+  </Button>
+              </View>
+            </View>
         </View>
     </SafeAreaView>
   );
