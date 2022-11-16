0e997c74f3ea302aa436bdff16de5b26b936d655
João-BenedetMachado <105388117+joaobenedetmachado@users.noreply.github.com>
2022-11-16 15:05:01 -0300

    Add files via upload

---
+++ react/App.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Provider as PaperProvider} from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from './screens/Login';
+import Problema from './screens/Problema';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="HarmoFit"
+          screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: { backgroundColor: '#000000' },
+            headerTitleStyle: { fontWeight: 'bold', color: '#fff' },
+          }}>
+          <Stack.Screen name="HarmoFit" component={Login} />
+          <Stack.Screen name="Problema" component={Problema} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
